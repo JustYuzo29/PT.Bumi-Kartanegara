4d7ae092cfae6097e5edee70c585e83d831e3ebf
JekiSkibidi <dzaky020705@gmail.com>
2025-07-30 17:44:54 +0800

    ADMIN

---
+++ frontend/src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-8 bg-blue-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello from Tailwind + React!
+      </h1>
+    </div>
+  )
+}
+
+export default App
