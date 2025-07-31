fc7c58e39312f27d3b2322f1cb15d7ce807bd2c3
JekiSkibidi <dzaky020705@gmail.com>
2025-07-31 18:06:24 +0800

    ADMIN

--- frontend/src/components/AdminPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AdminPage() {
-  return (
-    <div className="h-screen flex items-center justify-center text-2xl font-bold">
-      Selamat Datang di Halaman Admin!
-    </div>
-  );
-}
